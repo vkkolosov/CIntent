d47946688b2e1b59ca20d38e48dc1f76faf5450f
Vitaliy Kolosov <vk.kolosov@gmail.com>
2023-08-14 00:28:34 +0300

    callback

--- app/src/main/java/com/gamecodeschool/cintent/CrimeListFragment.kt
+++ app/src/main/java/com/gamecodeschool/cintent/CrimeListFragment.kt
@@ -1,5 +1,6 @@
 package com.gamecodeschool.cintent
 
+import android.content.Context
 import android.icu.text.DateFormat
 import android.os.Build
 import android.os.Bundle
@@ -17,16 +18,37 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.gamecodeschool.cintent.CrimeListFragment.Companion.newInstance
 import java.util.*
 
 private const val TAG = "CrimeListFragment"
 //удалили  onCreate : Activity, непонятно почему
 class CrimeListFragment : Fragment() {
 
+    //Интерфейс обратного вызова позволяет передавать события
+    //кликов из CrimeListFragment обратно на хост-activity
+    interface Callbacks {
+        fun onCrimeSelected(crimeId: UUID)
+    }
+
+    private var callbacks: Callbacks? = null
     private lateinit var crimeRecyclerView: RecyclerView
     private var adapter: CrimeAdapter? = CrimeAdapter(emptyList())
     private val crimeListViewModel: CrimeListViewModel by lazy {
         ViewModelProviders.of(this).get(CrimeListViewModel::class.java)
+    }
+
+    //Activity является подклассом Context,
+    //поэтому функция onAttach(...) передает в качестве
+    //параметра объект Context, который более гибок
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        callbacks = context as Callbacks?
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        callbacks = null
     }
 
     //создание View
@@ -40,12 +62,14 @@ class CrimeListFragment : Fragment() {
         //RecyclerView не отображает элементы на самом экране. Он передает эту задачу объекту LayoutManager.
         crimeRecyclerView.layoutManager = LinearLayoutManager(context)
         crimeRecyclerView.adapter = adapter
+        //updateUI() //пока тут
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        updateUI() //что тут, то там не сильно много разницы. onViewCreated сдеално, чтобы можно было ожидать в UI подгрузку с БД
+        /*
         crimeListViewModel.crimeListLiveData.observe(
             viewLifecycleOwner,
             Observer { crimes ->
@@ -54,10 +78,12 @@ class CrimeListFragment : Fragment() {
                     updateUI(crimes)
                 }
             })
+         */
 
     }
 
-    private fun updateUI(crimes: List<Crime>) {
+    private fun updateUI() { //было private fun updateUI(crimes: List<Crime>)
+        val crimes = crimeListViewModel.crimes
         adapter = CrimeAdapter(crimes)
         crimeRecyclerView.adapter = adapter
     }
@@ -65,8 +91,7 @@ class CrimeListFragment : Fragment() {
     //RecyclerView никогда не создает объекты View сам по
     //себе. Он всегда создает ViewHolder, которые выводят свои
     //itemView
-    private inner class CrimeHolder(view: View) : RecyclerView.ViewHolder(view),
-        View.OnClickListener {
+    private inner class CrimeHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
 
         private lateinit var crime: Crime
         private val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
@@ -98,7 +123,8 @@ class CrimeListFragment : Fragment() {
         }
 
         override fun onClick(v: View?) {
-            Toast.makeText(context, "${crime.title} pressed!", Toast.LENGTH_SHORT).show()
+            callbacks?.onCrimeSelected(crime.id)
+            //Toast.makeText(context, "${crime.title} pressed!", Toast.LENGTH_SHORT).show()
         }
     }
 
--- app/src/main/java/com/gamecodeschool/cintent/MainActivity.kt
+++ app/src/main/java/com/gamecodeschool/cintent/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.gamecodeschool.cintent
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+private const val TAG = "MainActivity"
+
+//Activity является подклассом Context
+class MainActivity : AppCompatActivity(), CrimeListFragment.Callbacks {
 
     private lateinit var crime: Crime
 
@@ -21,6 +26,23 @@ class MainActivity : AppCompatActivity() {
         }
 
         crime = Crime()
+    }
+
+    override fun onCrimeSelected(crimeId: UUID)
+    {
+        Log.d(TAG, "MainActivity.onCrimeSelected: $crimeId")
+        val fragment = CrimeFragment()
+        //Функция
+        //FragmentTransaction.replace(Int,Fragment) заменяет
+        //фрагмент, размещенный в activity (в контейнере с указанным
+        //целочисленным идентификатором ресурса)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            //при нажатии пользователем кнопки «Назад»
+            //транзакция будет обращена
+            .addToBackStack(null) //название состояния обратного стека
+            .commit()
     }
 
 }
